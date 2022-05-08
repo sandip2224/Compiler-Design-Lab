2cd2942a32b1d11f2e958bde636128cf39d9af46
sandip2224 <sandipan2224@gmail.com>
2022-05-08 10:27:46 +0530

    added code

---
+++ ai/bfs.py
@@ -0,0 +1,30 @@
+import collections
+
+
+def bfs(graph, root):
+    visited = set()
+    # queue with root element
+    q = collections.deque([root])
+
+    while q:
+        node = q.popleft()
+        visited.add(node)
+
+        for i in graph[node]:
+            if i not in visited:
+                q.append(i)
+
+    print(visited)
+
+
+if __name__ == '__main__':
+    # dictionery
+    graph = {
+        0: [1, 2, 3],
+        1: [0, 2],
+        2: [0, 1, 4],
+        3: [0],
+        4: [2]
+    }
+
+    bfs(graph, 0)
